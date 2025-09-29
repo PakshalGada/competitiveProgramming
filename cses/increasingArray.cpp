7eb715f046339e0288b275e39b107b07cc170ce2
PakshalGada <pakshalgada5@gmail.com>
2025-09-29 19:41:11 +0530

    newProblems

---
+++ cses/increasingArray.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+using ll = long long;
+
+int main() {
+    
+    int n;
+    cin>>n;
+    vector<ll> a(n);
+    
+    for (int i=0; i<n; i++) {
+        cin>>a[i];
+    }
+    
+    ll count=0;
+    for(int i=1; i<n ; i++) {
+        if (a[i]<a[i-1]) {
+            count += (a[i-1]-a[i]);
+            a[i]=a[i-1];
+        }
+    }
+    cout<<count;
+    return 0;
+}
