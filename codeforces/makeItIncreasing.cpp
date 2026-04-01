b8fe979b74e6af5eab567a2d2556facdc1a75068
PakshalGada <pakshalgada5@gmail.com>
2026-04-01 22:33:14 +0530

    update

--- codeforces/makeItIncreasing.cpp
+++ codeforces/makeItIncreasing.cpp
@@ -21,6 +21,33 @@ typedef pair<int, int> pii;
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
-    
-    
+
+    int t; cin>>t;
+
+    while(t--){
+        int n; cin>>n;
+
+        vi a(n);
+        int ans = 0;
+        bool flag = true;
+        if (n == 1) {
+            cout << 0 << '\n';
+            continue;
+        }
+        for(int i=0; i<n; i++) cin>>a[i];
+
+        for (int i = n-2; i>=0; i--){
+            while(a[i]>=a[i+1] && a[i]>0){
+                a[i]/=2;
+                ans++;
+            }
+            if(a[i]==a[i+1]){
+                flag = false;
+                break;
+            }
+        }
+        if(flag) cout<<ans<<endl;
+        else cout<<-1<<endl;
+    }
+
 }
