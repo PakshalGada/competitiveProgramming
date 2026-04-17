81b35fcea9202ac01a239e4e9dbad2d736b88f0d
Pakshal Gada <pakshalgada5@gmail.com>
2026-04-17 20:53:29 +0530

    update

--- codeforces/omkarAndLastClassOfMath.cpp
+++ codeforces/omkarAndLastClassOfMath.cpp
@@ -9,6 +9,7 @@ typedef pair<int, int> pii;
 #define all(x) (x).begin(), (x).end()
 #define pb push_back
 #define mp make_pair
+
 #define fi first
 #define se second
 
@@ -21,6 +22,31 @@ typedef pair<int, int> pii;
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
-    
-    
+
+    int t; cin>>t;
+    while(t--){
+
+        ll n; cin>>n;
+
+        if(n%2==0){
+            cout<<n/2<<" "<<n/2<<endl;
+        }else{
+            ll d = -1;
+            for(ll i=3; i*i<=n; i+=2){
+                if(n%i==0){
+                    d=i;
+                    break;
+                }
+            }
+
+            if(d==-1){
+                cout<<1<<" "<<n-1<<endl;
+            } else {
+                ll a = n/d;
+                cout<<a<<" "<<n-a<<endl;
+            }
+        }
+
+    }
+
 }
