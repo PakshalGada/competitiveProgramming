1b3ae745830b0428b2b83638116542d73b7078cd
Pakshal Gada <pakshalgada5@gmail.com>
2026-04-19 19:47:55 +0530

    update

---
+++ cses/ferrisWheel.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef pair<int, int> pii;
+
+#define all(x) (x).begin(), (x).end()
+#define pb push_back
+#define mp make_pair
+
+#define fi first
+#define se second
+
+#ifdef LOCAL
+#define debug(x) cerr << #x << " = " << (x) << endl
+#else
+#define debug(x)
+#endif
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    ll n, x; cin>>n>>x;
+
+    vll a(n);
+
+    for(ll i=0; i<n; i++) cin>>a[i];
+
+    sort(all(a));
+
+    int l=0, r=n-1;
+    int ans=0;
+
+    while(l<=r){
+        if(a[l]+a[r]<=x){
+            l++;
+        }
+
+        r--;
+        ans++;
+    }
+
+    cout<<ans<<endl;
+
+}
