31b264be91bad331324d15106c31be6ce3669984
Pakshal Gada <pakshalgada5@gmail.com>
2026-04-24 20:09:48 +0530

    update

---
+++ codeforces/magnets.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef pair<int, int> pii;
+
+#define all(x) (x).begin(), (x).end()
+#define pb push_back
+#define mp make_pair
+
+#define fi first
+#define se second
+
+#ifdef LOCAL
+#define debug(x) cerr << #x << " = " << (x) << endl
+#else
+#define debug(x)
+#endif
+
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int n; cin>>n;
+
+    string prev, curr;
+    cin>>prev;
+
+    int groups = 1;
+
+    for (int i=1; i<n; i++) {
+        cin>>curr;
+        if (curr!=prev) groups++;
+        prev = curr;
+    }
+
+    cout<<groups<<endl;
+}
