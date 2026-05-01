f8d6e19355a55ca3867634a0dae4381869cb5627
Pakshal Gada <pakshalgada5@gmail.com>
2026-05-01 15:50:51 +0530

    update

--- cses/playlist.cpp
+++ cses/playlist.cpp
@@ -2,13 +2,16 @@
 using namespace std;
 
 typedef long long ll;
+typedef long double ld;
 typedef vector<int> vi;
 typedef vector<ll> vll;
 typedef pair<int, int> pii;
 
 #define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
 #define pb push_back
 #define mp make_pair
+
 #define fi first
 #define se second
 
@@ -18,9 +21,43 @@ typedef pair<int, int> pii;
 #define debug(x)
 #endif
 
+const ll MOD = 1000000007;
+
+bool isPrime(int x){
+    if(x<=1) return false;
+
+    for (int i=2; i*i<=x; i++) {
+        if (x%i==0) {
+            return false;
+        }
+    }
+    return true;
+
+
+}
+
+
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
-    
-    
+
+    int n; cin>>n;
+
+    vi a(n);
+
+    for(int i=0; i<n; i++) cin>>a[i];
+
+    set<int> s;
+
+    int i=0, j=0, ans=0;
+    while (i<n && j<n) {
+        if (s.find(a[j]) == s.end()) {
+            s.insert(a[j++]);
+            ans = max(ans, j - i);
+        } else {
+            s.erase(a[i++]);
+        }
+    }
+
+    cout<<ans<<endl;
 }
