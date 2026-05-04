191d6dde0c2ad7492a49a3b0290d908b6175227e
Pakshal Gada <pakshalgada5@gmail.com>
2026-05-04 19:52:30 +0530

    update

---
+++ codeforces/phoenixAndPuzzle.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef pair<int, int> pii;
+
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define pb push_back
+#define mp make_pair
+
+#define fi first
+#define se second
+
+#ifdef LOCAL
+#define debug(x) cerr << #x << " = " << (x) << endl
+#else
+#define debug(x)
+#endif
+
+const ll MOD = 1000000007;
+const int MAX_VAL = 1e6;
+
+bool isSquare(int x){
+    int y=sqrt(x);
+     return y*y==x;
+}
+
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
+    int t; cin>>t;
+
+    while(t--){
+        ll n; cin>>n;
+
+        if(n%2==0 && isSquare(n/2)){
+            cout<<"YES"<<endl;
+        }else if(n%4==0 && isSquare(n/4)){
+            cout<<"YES"<<endl;
+        }else{
+            cout<<"NO"<<endl;
+        }
+    }
+}
