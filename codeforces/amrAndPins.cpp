507fd94a46beb4cf5bf568b62ca3025843cc3f0e
Pakshal Gada <pakshalgada5@gmail.com>
2026-05-05 20:53:02 +0530

    update

--- codeforces/amrAndPins.cpp
+++ codeforces/amrAndPins.cpp
@@ -2,13 +2,16 @@
 using namespace std;
 
 typedef long long ll;
+typedef long double ld;
 typedef vector<int> vi;
 typedef vector<ll> vll;
 typedef pair<int, int> pii;
 
 #define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
 #define pb push_back
 #define mp make_pair
+
 #define fi first
 #define se second
 
@@ -18,9 +21,17 @@ typedef pair<int, int> pii;
 #define debug(x)
 #endif
 
+const ll MOD = 1000000007;
+const int MAX_VAL = 1e6;
+
+
 int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    
-    
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
+    ll r, x, y, x1, y1; cin>>r>>x>>y>>x1>>y1;
+
+    double dist = sqrt(((x1-x)*(x1-x))+((y1-y)*(y1-y)));
+
+    cout<<ceil(dist/(2*r))<<endl;
 }
