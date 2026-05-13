3d78c7ec326ed1e3fb232eed02b05d81d47569cd
Pakshal Gada <pakshalgada5@gmail.com>
2026-05-13 14:22:38 +0530

    update

---
+++ codeforces/newYearAndHurry.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef pair<int, int> pii;
+
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define pb push_back
+#define mp make_pair
+
+#define fi first
+#define se second
+
+#ifdef LOCAL
+#define debug(x) cerr << #x << " = " << (x) << endl
+#else
+#define debug(x)
+#endif
+
+const ll MOD = 1000000007;
+const int MAX_VAL = 1e6;
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
+
+    int n, k; cin>>n>>k;
+
+    int time=240-k;
+
+    int ans=0;
+    int spent=0;
+
+    for(int i=1; i<=n; i++){
+        spent+=5*i;
+
+        if(spent<=time) ans++;
+        else break;
+    }
+
+    cout<<ans<<endl;
+}
