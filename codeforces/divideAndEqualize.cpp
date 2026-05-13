6d1d3582044ab3ede6eaf856556a372e3a6191ff
Pakshal Gada <pakshalgada5@gmail.com>
2026-05-13 14:06:20 +0530

    update

---
+++ codeforces/divideAndEqualize.cpp
@@ -0,0 +1,65 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef pair<int, int> pii;
+
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define pb push_back
+#define mp make_pair
+
+#define fi first
+#define se second
+
+#ifdef LOCAL
+#define debug(x) cerr << #x << " = " << (x) << endl
+#else
+#define debug(x)
+#endif
+
+const ll MOD = 1000000007;
+const int MAX_VAL = 1e6;
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
+
+    int t; cin>>t;
+
+    while(t--){
+        int n; cin>>n;
+
+        unordered_map<int, int> prime;
+
+        for(int i=0; i<n; i++){
+            int x; cin>>x;
+
+            for(int p=2; p*p<=x; p++){
+                while(x%p==0){
+                    prime[p]++;
+                    x/=p;
+                }
+            }
+
+            if(x>1){
+                prime[x]++;
+            }
+        }
+
+        bool flag=true;
+
+        for(auto &[p, count]:prime){
+            if(count%n!=0){
+                flag = false;
+                break;
+            }
+        }
+
+        cout<<(flag?"YES":"NO")<<endl;
+    }
+}
