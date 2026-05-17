e200dd42f47b3c10579005438461447e06a74877
Pakshal Gada <pakshalgada5@gmail.com>
2026-05-17 12:32:53 +0530

    update

---
+++ codeforces/dungeon.cpp
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef pair<int, int> pii;
+
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define pb push_back
+#define mp make_pair
+
+#define fi first
+#define se second
+
+#ifdef LOCAL
+#define debug(x) cerr << #x << " = " << (x) << endl
+#else
+#define debug(x)
+#endif
+
+const ll MOD = 1000000007;
+const int MAX_VAL = 1e6;
+
+bool isPrime(ll n) {
+    if (n<2) return false;
+    for (ll i=2; i*i<=n; i++) {
+        if (n%i==0) return false;
+    }
+    return true;
+}
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
+
+    int t; cin>>t;
+
+    while(t--){
+        ll a, b, c;cin>>a>>b>>c;
+
+        ll sum = a+b+c;
+
+        if(sum%9!=0){
+            cout<<"NO"<<endl;
+        }else{
+            if(min(a,min(b,c))>=(sum/9)){
+                cout<<"YES"<<endl;
+            } else {
+                cout<<"NO"<<endl;
+            }
+        }
+    }
+}
