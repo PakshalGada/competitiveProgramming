448b211ac2672671d62bbdf779f411228e60a013
Pakshal Gada <pakshalgada5@gmail.com>
2026-05-23 16:57:15 +0530

    update

---
+++ codeforces/sherlockAndHisGirlfriend.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef pair<int, int> pii;
+
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define pb push_back
+#define mp make_pair
+
+#define fi first
+#define se second
+
+#ifdef LOCAL
+#define debug(x) cerr << #x << " = " << (x) << endl
+#else
+#define debug(x)
+#endif
+
+const ll MOD = 1000000007;
+const int MAX_VAL = 1e6;
+
+bool isPrime(ll n) {
+    if (n<2) return false;
+    for (ll i=2; i*i<=n; i++) {
+        if (n%i==0) return false;
+    }
+    return true;
+}
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
+    int n; cin>>n;
+
+    if(2>=n) cout<<1<<endl;
+    else cout<<2<<endl;
+
+    for(int i=2; i<=n+1; i++){
+        if(isPrime(i)) cout<<1<<" ";
+        else cout<<2<<" ";
+    }
+
+    cout<<endl;
+
+}
