5b6aaf1f5b650674bd5bf230e163d14a14e6c7be
Pakshal Gada <pakshalgada5@gmail.com>
2026-05-27 21:15:35 +0530

    new question

---
+++ codeforces/fadiAndLcm.cpp
@@ -0,0 +1,55 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef pair<int, int> pii;
+
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define pb push_back
+#define mp make_pair
+
+#define fi first
+#define se second
+
+#ifdef LOCAL
+#define debug(x) cerr << #x << " = " << (x) << endl
+#else
+#define debug(x)
+#endif
+
+const ll MOD = 1000000007;
+const int MAX_VAL = 1e6;
+
+bool isPrime(ll n) {
+    if (n<2) return false;
+    for (ll i=2; i*i<=n; i++) {
+        if (n%i==0) return false;
+    }
+    return true;
+}
+
+ll lcm(ll a, ll b){
+    return a / __gcd(a, b)*b;
+}
+
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
+    ll x; cin>>x;
+
+    ll ans;
+    for(ll i=1; i*i<=x; i++){
+        if(x%i==0 && lcm(i, x/i)==x){
+            ans = i;
+        }
+    }
+    cout<<ans<<" "<<x/ans<<endl;
+
+
+}
