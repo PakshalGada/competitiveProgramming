03051d55e2093dea8bbba7ca3f34f797f8a5fe77
Pakshal Gada <pakshalgada5@gmail.com>
2026-05-29 23:54:14 +0530

    new question

--- codeforces/restoringThreeNumbers.cpp
+++ codeforces/restoringThreeNumbers.cpp
@@ -2,13 +2,16 @@
 using namespace std;
 
 typedef long long ll;
+typedef long double ld;
 typedef vector<int> vi;
 typedef vector<ll> vll;
 typedef pair<int, int> pii;
 
 #define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
 #define pb push_back
 #define mp make_pair
+
 #define fi first
 #define se second
 
@@ -18,9 +21,32 @@ typedef pair<int, int> pii;
 #define debug(x)
 #endif
 
+const ll MOD = 1000000007;
+const int MAX_VAL = 1e6;
+
+bool isPrime(ll n) {
+    if (n<2) return false;
+    for (ll i=2; i*i<=n; i++) {
+        if (n%i==0) return false;
+    }
+    return true;
+}
+
+ll lcm(ll a, ll b){
+    return a / __gcd(a, b)*b;
+}
+
+
 int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    
-    
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
+
+    vi a(4);
+    for (int i=0; i<4; i++) cin>>a[i];
+    sort(all(a));
+    cout<<a[3]-a[0]<<" "<<a[3]-a[1]<<" "<<a[3]-a[2]<<endl;
+
+
+
 }
