e9da02adfe29d252c58e895247ca28fcc5d99a88
Pakshal Gada <pakshalgada5@gmail.com>
2026-06-30 22:51:47 +0530

    new questions

--- codeforces/alternativeWay.cpp
+++ codeforces/alternativeWay.cpp
@@ -2,13 +2,17 @@
 using namespace std;
 
 typedef long long ll;
+typedef long double ld;
 typedef vector<int> vi;
 typedef vector<ll> vll;
 typedef pair<int, int> pii;
+typedef pair<long long, long long> pll;
 
 #define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
 #define pb push_back
 #define mp make_pair
+
 #define fi first
 #define se second
 
@@ -18,9 +22,117 @@ typedef pair<int, int> pii;
 #define debug(x)
 #endif
 
+const ll MOD = 1000000007;
+const int MAX_VAL = 1e6;
+const int MAX = 200007;
+
+const int MAXN = 1e6 + 5;
+
+vector<bool> is_prime(MAXN, true);
+vector<int> primes;
+
+void sieve() {
+    is_prime[0] = is_prime[1] = false;
+
+    for (int i = 2; i * i < MAXN; i++) {
+        if (is_prime[i]) {
+            for (int j = i * i; j < MAXN; j += i) {
+                is_prime[j] = false;
+            }
+        }
+    }
+
+    for (int i = 2; i < MAXN; i++) {
+        if (is_prime[i]) primes.push_back(i);
+    }
+}
+
+
+bool isPrime(ll n) {
+    if (n<2) return false;
+    for (ll i=2; i*i<=n; i++) {
+        if (n%i==0) return false;
+    }
+    return true;
+}
+
+ll lcm(ll a, ll b){
+    return a / __gcd(a, b)*b;
+}
+
+ll sum(ll x){
+    return x*(x+1)/2;
+}
+
+bool isPalindrome(ll n) {
+    string s = to_string(n);
+    string r = s;
+    reverse(all(r));
+    return s == r;
+}
+
+ll digSum(ll n){
+    ll sum=0;
+    while(n>0){
+        sum+=n%10;
+        n/=10;
+    }
+    return sum;
+}
+
+ll cnt3(ll x){
+    int count=0;
+    while(x%3==0){
+        count++;
+        x/=3;
+    }
+    return count;
+}
+
+vector<vi> adj;
+vector<bool> vis;
+
+void dfs(int u) {
+    vis[u] = true;
+    for (int v : adj[u]) {
+        if (!vis[v]) dfs(v);
+    }
+}
+
+ll isGood(ll x){
+    set<char> digits;
+    string s = to_string(x);
+    for (char c:s) digits.insert(c);
+    return digits.size() <= 2;
+}
+
+
 int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    
-    
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
+    int tt; cin>>tt;
+
+    while(tt--){
+
+        int n; cin>>n;
+
+        vll a(n), b(n);
+
+        for(ll i=0; i<n; i++) cin>>a[i];
+        for(ll i=0; i<n; i++) cin>>b[i];
+
+        ll diffSum=0;
+        bool flag=true;
+        for(int i=0; i<n; i++){
+            diffSum+=b[i]-a[i];
+            if(diffSum<0) flag=false;
+        }
+
+        cout<<(flag?"YES":"NO")<<endl;
+
+    }
+
+
+
 }
