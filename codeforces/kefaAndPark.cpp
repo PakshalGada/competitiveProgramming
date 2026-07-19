facca913353c7dd69e2b53b27635c271e543c148
Pakshal Gada <pakshalgada5@gmail.com>
2026-07-19 19:07:45 +0530

    new question

---
+++ codeforces/kefaAndPark.cpp
@@ -0,0 +1,117 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef pair<int, int> pii;
+typedef pair<long long, long long> pll;
+
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define pb push_back
+#define mp make_pair
+
+#define fi first
+#define se second
+
+#ifdef LOCAL
+#define debug(x) cerr << #x << " = " << (x) << endl
+#else
+#define debug(x)
+#endif
+
+const ll MOD = 1000000007;
+const int MAX_VAL = 1e6;
+const int MAX = 200007;
+
+const int MAXN = 1e6 + 5;
+
+vector<bool> is_prime(MAXN, true);
+vector<int> primes;
+
+void sieve() {
+    is_prime[0] = is_prime[1] = false;
+
+    for (int i = 2; i * i < MAXN; i++) {
+        if (is_prime[i]) {
+            for (int j = i * i; j < MAXN; j += i) {
+                is_prime[j] = false;
+            }
+        }
+    }
+
+    for (int i = 2; i < MAXN; i++) {
+        if (is_prime[i]) primes.push_back(i);
+    }
+}
+
+
+bool isPrime(ll n) {
+    if (n<2) return false;
+    for (ll i=2; i*i<=n; i++) {
+        if (n%i==0) return false;
+    }
+    return true;
+}
+
+ll lcm(ll a, ll b){
+    return a / __gcd(a, b)*b;
+}
+
+long long factorial(int n){
+    long long f = 1;
+    for(int i=2; i<=n; i++) f*=i;
+    return f;
+}
+
+vi adj[MAXN];
+int cat[MAXN];
+int n, m;
+int ans = 0;
+
+void dfs(int node, int parent, int consecutiveCats) {
+
+    if (cat[node] == 1) consecutiveCats++;
+    else consecutiveCats = 0;
+
+    if (consecutiveCats > m) return;
+
+    bool isLeaf = true;
+    for (int neighbor : adj[node]) {
+        if (neighbor != parent) {
+            isLeaf = false;
+            dfs(neighbor, node, consecutiveCats);
+        }
+    }
+
+    if (isLeaf && node != 1) {
+        ans++;
+    }
+}
+
+
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
+    cin>>n>>m;
+
+    for(int i=1; i<=n; i++) cin>>cat[i];
+
+    for(int i=0; i<n-1; i++){
+        int x, y; cin>>x>>y;
+
+        adj[x].pb(y);
+        adj[y].pb(x);
+    }
+
+    dfs(1,0,0);
+
+    cout<<ans<<endl;
+
+
+
+}
