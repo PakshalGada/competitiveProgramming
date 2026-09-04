0d919c7a423a3593fc82bde9f7172d0560b3788a
PakshalGada <pakshalgada5@gmail.com>
2026-09-04 19:38:29 +0530

    new question

--- cses/concertTickets.cpp
+++ cses/concertTickets.cpp
@@ -2,11 +2,15 @@
 using namespace std;
 
 typedef long long ll;
+typedef long double ld;
 typedef vector<int> vi;
 typedef vector<ll> vll;
-typedef pair<int, int> pii;
+typedef pair<int,int> pii;
+typedef pair<long long,long long> pll;
 
 #define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define forn(i, n) for (int i = 0; i < int(n); i++)
 #define pb push_back
 #define mp make_pair
 #define fi first
@@ -18,9 +22,72 @@ typedef pair<int, int> pii;
 #define debug(x)
 #endif
 
+const int INF = 1e9;
+const ll LINF = 4e18;
+const int MOD = 1e9 + 7;
+const int MAXN = 1e6 + 5;
+
+vector<bool> is_prime(MAXN, true);
+
+vector<int> primes;
+
+void sieve() {
+    is_prime[0] = is_prime[1] = false;
+    for (int i = 2; i * i < MAXN; i++) {
+        if (is_prime[i]) {
+            for (int j = i * i; j < MAXN; j += i)
+                is_prime[j] = false;
+        }
+    }
+    for (int i = 2; i < MAXN; i++)
+        if (is_prime[i]) primes.push_back(i);
+}
+
+
+ll lcm(ll a, ll b) {
+    return a / __gcd(a, b) * b;
+}
+
+ll factorial(int n) {
+    ll f = 1;
+    for (int i = 2; i <= n; i++) f *= i;
+    return f;
+}
+
+
+
+
 int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    
-    
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
+
+    int n, m; cin>>n>>m;
+
+    multiset<int> a;
+
+    for(int i=0; i<n; i++){
+        int x; cin>>x;
+        a.insert(x);
+    }
+
+
+    for(int i=0; i<m; i++){
+        int y; cin>>y;
+
+        auto it=a.upper_bound(y);
+
+        if(it==a.begin()){
+            cout<<-1<<"\n";
+        }else{
+            --it;
+            cout<<*it<<"\n";
+            a.erase(it);
+        }
+    }
+
+
+
+
+
 }
